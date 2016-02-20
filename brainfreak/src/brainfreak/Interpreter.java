979ec6dfbc196b2f5ef19d843ed0c483363d2327
Joe Apicella <apicellaj@gmail.com>
2016-02-20 22:29:33 +0900

    Switch hasValidBrackets method to use a regex String and format code

--- brainfreak/src/brainfreak/Interpreter.java
+++ brainfreak/src/brainfreak/Interpreter.java
@@ -7,7 +7,7 @@ class Interpreter {
     //TODO: maybe create an enum for the different array bit states
     //TODO: maybe convert BF string with run length encoding (RLE)* for optimization
     //TODO: reduce scope as much as possible, use final modifier where possible
-    //TODO: allow string to be sent out after each write command, use StringBuilder?
+    //TODO: find a more efficient way to write to resultArea
     //TODO: allow operation to be cancelled with a button (halt button)
     
     private int memoryPosition = 0;
@@ -16,7 +16,7 @@ class Interpreter {
     private final String code;
     private StringBuilder result = new StringBuilder();
     private long numberOfCalculations = 0L;
-    private static final long MAX_CALCULATIONS_ALLOWED = Long.MAX_VALUE;
+    private static final long MAX_CALCULATIONS_ALLOWED = Integer.MAX_VALUE;
     private static final int MEMORY_SIZE = 30000;
     private int[] inputArray;
     private int[] memoryArray = new int[MEMORY_SIZE];
@@ -73,13 +73,8 @@ class Interpreter {
     
     private void interpret(char c) {
         if (c == ';') {
-            if (inputPosition >= inputArray.length) {
-                System.out.println("\n\nERROR: Insufficient input data provided.");
-                System.exit(-1);
-            } else {
-                memoryArray[memoryPosition] = inputArray[inputPosition];
-                inputPosition++;
-            }
+        	memoryArray[memoryPosition] = inputArray[inputPosition];
+            inputPosition++;
         }
         if (c == ':') result.append(memoryArray[memoryPosition]).append(' ');
         if (c == ',') {
@@ -93,7 +88,6 @@ class Interpreter {
             }
             char value = (char) memoryArray[memoryPosition];
             result.append(value);
-            //TODO:perhaps use this as an opportunity to write to the GUI
         }
         if (c == '>') {
             memoryPosition++;
@@ -114,27 +108,38 @@ class Interpreter {
     
     private void enterLoop(int startOfLoop) {
         int loopExit = loopExit(startOfLoop);
-        while(memoryArray[memoryPosition] != 0) decode(startOfLoop);
+        while(memoryArray[memoryPosition] != 0) {
+        	decode(startOfLoop);
+        }
         codePosition = loopExit;
     }
     
     private int loopExit(int startOfLoop) {
         Stack<Character> s = new Stack<>();
         for (int j = startOfLoop; j < code.length(); j++) {
-            if (code.charAt(j) == '[') s.add(code.charAt(j));
-            if (code.charAt(j) == ']' && !s.isEmpty()) s.pop();
-            else if (code.charAt(j) == ']' && s.isEmpty()) return j;
+            if (code.charAt(j) == '[') {
+            	s.add(code.charAt(j));
+            }
+            if (code.charAt(j) == ']' && !s.isEmpty()) {
+            	s.pop();
+            } else if (code.charAt(j) == ']' && s.isEmpty()) {
+            	return j;
+            }
         }
         result.append("\n\nERROR: Looping problem.");
         return code.length();
     }
     
     private boolean hasValidBrackets() {
+    	final String bracketText = code.replaceAll("[^\\[\\]]", "");
         Stack<Character> s = new Stack<>();
-        for (int pos = 0; pos < code.length(); pos++) {
-            if (code.charAt(pos) == '[') s.add(code.charAt(pos));
-            if (code.charAt(pos) == ']' && !s.isEmpty()) s.pop();
-            else if (code.charAt(pos) == ']' && s.isEmpty()) {
+        for (int pos = 0; pos < bracketText.length(); pos++) {
+            if (bracketText.charAt(pos) == '[') {
+            	s.add('[');
+            }
+            if (bracketText.charAt(pos) == ']' && !s.isEmpty()) {
+            	s.pop();
+            } else if (bracketText.charAt(pos) == ']' && s.isEmpty()) {
                return false; 
             }
         }
