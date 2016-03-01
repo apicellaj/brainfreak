1ba9ca32793e11f9bcb0c5556b5d18b50ccf520c
Joe Apicella <apicellaj@gmail.com>
2016-03-01 21:51:25 +0900

    Add more Interpreter unit tests

--- brainfreak/src/test/java/brainfreak/InterpreterTest.java
+++ brainfreak/src/test/java/brainfreak/InterpreterTest.java
@@ -11,30 +11,84 @@ import org.junit.Test;
 import main.java.brainfreak.Interpreter;
 
 public class InterpreterTest {
-  
-  private String badBrackets = "][]";
-  private Interpreter interpreter = new Interpreter(badBrackets, "", false, null);
 
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-  }
+	private Interpreter interpreter;
+	
+	private final static boolean MEMORY_WRAP_OFF = false;
+	private final static boolean MEMORY_WRAP_ON = true;
+	private final static String NO_STD_IN = "";
 
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {
-  }
+	private String badBrackets;
+	private String unusedInput;
+	private String memoryWrap;
+	private String helloWorld;
 
-  @Before
-  public void setUp() throws Exception {
-  }
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
 
-  @After
-  public void tearDown() throws Exception {
-  }
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
 
-  @Test
-  public void test() {
-    interpreter.run();
-    assertEquals("ERROR: Loop brackets paired incorrectly.\n", interpreter.getResult());
-  }
+	@Before
+	public void setUp() throws Exception {
+		badBrackets = "][]";
+		unusedInput = "1 2 3 4";
+		memoryWrap = "<++++[-<++++++++>]<+.";
+		helloWorld = ">++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->"
+					+ "+++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+";
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testBrackets() {
+		interpreter = new Interpreter(badBrackets, NO_STD_IN, MEMORY_WRAP_OFF, null);
+		interpreter.run();
+		assertEquals("ERROR: Loop brackets paired incorrectly.\n", interpreter.getResult());
+	}
+
+	@Test
+	public void testUnusedInput() {
+		interpreter = new Interpreter("", unusedInput, MEMORY_WRAP_OFF, null);
+		interpreter.run();
+		assertEquals("WARNING: Unused input data.\n", interpreter.getResult());
+	}
+	
+	@Test
+	public void testInsufficientInput() {
+		interpreter = new Interpreter(",", NO_STD_IN, MEMORY_WRAP_OFF, null);
+		interpreter.run();
+		assertEquals("ERROR: Insufficient input data.\n", interpreter.getResult());
+	}
+
+	@Test
+	public void testMemoryWrapOff() {
+		interpreter = new Interpreter(memoryWrap, NO_STD_IN, MEMORY_WRAP_OFF, null);
+		interpreter.run();
+		assertEquals("ERROR: Memory Underflow at character 0\n", interpreter.getResult());
+	}
+
+	@Test
+	public void testMemoryWrapOn() {
+		interpreter = new Interpreter(memoryWrap, NO_STD_IN, MEMORY_WRAP_ON, null);
+		interpreter.run();
+		assertEquals("!", interpreter.getResult());
+	}
+
+	@Test
+	public void testHelloWorld() {
+		interpreter = new Interpreter(helloWorld, NO_STD_IN, MEMORY_WRAP_OFF, null);
+		interpreter.run();
+		assertEquals("Hello World!", interpreter.getResult());
+	}
+
+	@Test
+	public void testExtendedMode() {
+		//TODO
+	}
 
 }
